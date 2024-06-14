97c217f61db3d4ebfd7a8003cd989d4cd5b2f425
Abdulrahman Awad <abdulrahmansawad@gmail.com>
2024-06-14 01:37:31 -0400

    font updates

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,6 +5,19 @@ const About = () => {
     <div className='about'>
         <div className='content'>
             <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
+            <h1>About</h1>
             {/* <button className='btn'>About<img src={rightArrow} alt=''></img></button> */}
         </div>
     </div>
