6db609f987778270f9d9b8daeaefd3a98165dcbd
Abdulrahman Awad <abdulrahmansawad@gmail.com>
2024-06-16 01:34:33 -0400

    Updated Projects Section

--- src/Components/MyProjects/MyProjects.jsx
+++ src/Components/MyProjects/MyProjects.jsx
@@ -2,13 +2,13 @@ import './MyProjects.css'
 
 import React from 'react'
 import project1_img from '../../assets/project1.png'
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-// import project2_img from '../../assets/project2.jpg'
-// import project3_img from '../../assets/project3.jpg'
-// import project4_img from '../../assets/project4.jpg'
-// import project5_img from '../../assets/project5.jpg'
-// import project6_img from '../../assets/project6.jpg'
+import project4_img from '../../assets/project4.jpg'
+import project3_img from '../../assets/project3.jpg'
+import project2_img from '../../assets/project2.png'
+import project5_img from '../../assets/project5.jpg'
+import project6_img from '../../assets/project6.jpg'
 
 const MyProjects = () => {
   return (
@@ -26,7 +26,7 @@ const MyProjects = () => {
       <div className="mywork-showmore">
         <p>More Projects</p>
         <div className='arrow-outward-icon'>
-          <ArrowOutwardIcon />
+          <ArrowForwardIosIcon />
         </div>
       </div>
     </div>
@@ -34,32 +34,32 @@ const MyProjects = () => {
 }
 
 const ProjectData = [
-  {
+  { /* VsCode Extension: Nokia WFM */
     w_no: 1,
     w_name: 'Project 1',
-    w_img: project1_img,
+    w_img: project6_img,
   },
-  {
+  { /* Personal AI Chatbot */
     w_no: 2,
     w_name: 'Project 2',
-    w_img: project1_img,
+    w_img: project2_img,
   },
-  {
+  { /* Relational Database optimiser - soccer database */
     w_no: 3,
     w_name: 'Project 3',
-    w_img: project1_img,
+    w_img: project3_img,
   },
-  {
+  { /* Chat Application with Web-Sockets */
     w_no: 4,
     w_name: 'Project 4',
-    w_img: project1_img,
+    w_img: project4_img,
   },
-  {
+  { /* Weather App */
     w_no: 5,
     w_name: 'Project 5',
-    w_img: project1_img,
+    w_img: project5_img,
   },
-  {
+  { /* Personal Portfolio */
     w_no: 6,
     w_name: 'Project 6',
     w_img: project1_img,
