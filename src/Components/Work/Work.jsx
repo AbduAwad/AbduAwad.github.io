2e63610816fd112e0af523958505b45d386afc20
Abdulrahman Awad <abdulrahmansawad@gmail.com>
2024-08-29 21:23:18 -0400

    updates to work

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import work1_img from '../../assets/work.png'
 import work2_img from '../../assets/work2.jpg'
 import work3_img from '../../assets/work3.jpg'
+import work4_img from '../../assets/wrk_img.jpg'
 
 const Work = () => {
   return (
@@ -31,24 +32,28 @@ const Work = () => {
 const WorkData = [
   {
     w_no: 1,
-    w_name: 'Network Automation Software Engineer Intern',
+    w_name:'Teaching Assistant - Data Structures & Algorithms',
+    w_img: work4_img,
+    w_description: 'September 2024 - Current',
+  },
+  {
+    w_no: 2,
+    w_name:'Network Automation Software Engineer Intern',
     w_img: work1_img,
     w_description: 'April 2024 - September 2024',
   },
   {
-    w_no: 2,
+    w_no: 3,
     w_name: 'Teaching Assistant - Python Procedural Programming',
     w_img: work2_img,
     w_description: 'September 2023 - April 2024',
   },
   {
-    w_no: 3,
+    w_no: 4,
     w_name: 'Delivery Automation Software Developer Intern',
     w_img: work3_img,
     w_description: 'April 2023 - September 2023',
   }
-
-
 ];
 
 export default Work
